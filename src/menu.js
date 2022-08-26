45fb81068a940cd36575fdf445920aa35d58f337
lukin <ultra-technologies-nikita@mail.ru>
2022-08-27 00:43:44 +0300

    Implemented menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,22 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
-
+  constructor(selector) {
+    super(selector);
+  }
+  add(...modules) {
+    for (const module of modules) {
+      this.el.insertAdjacentHTML('beforeend', module.toHTML());
+    }
+  }
+  open(event) {
+    if (this.el.hasChildNodes()) {
+      this.el.style.top = `${event.clientY}px`;
+      this.el.style.left = `${event.clientX}px`;
+      this.el.classList.add('open');
+    }
+  }
+  close() {
+    this.el.classList.remove('open');
+  }
 }
