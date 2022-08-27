9065843e9a9b78ef83e47a52d10ddc39026da312
Mardon <mardonberdiyev10@gmail.com>
2022-08-27 23:53:51 +0500

    taymer-module restart

---
+++ src/modules/taymer.module.js
@@ -0,0 +1,34 @@
+import {Module} from '../core/module'
+
+export class TimerModule extends Module {
+    trigger(){
+       
+          const itemId = setInterval(updateCountDown, 1000)
+          let time = Number(prompt('задайте время в секундах'))
+        let paragropth = document.createElement('div')
+        document.body.append(paragropth)
+        
+        function  updateCountDown(){
+          time = time < 10 ? '0' + time: time;
+        paragropth.innerHTML= time
+          time--;
+          if(time < 0){
+            time = 0
+            paragropth.innerHTML = 'Таймер окончен'
+            setTimeout(()=>{
+                clearInterval(itemId)
+                if(paragropth.innerHTML === 'Таймер окончен'){
+                    paragropth.remove()
+                }
+              },2000)
+          }
+          
+        }
+       
+      
+            
+        
+        }
+        
+    }
+    
