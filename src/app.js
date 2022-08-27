49797d1f9d66383a69b782a00159818a7812bedf
lukin <ultra-technologies-nikita@mail.ru>
2022-08-27 14:40:21 +0300

    Updated format

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import "./styles.css";
-import { ContextMenu } from "./menu";
+import './styles.css';
+import { ContextMenu } from './menu';
 
-const contextMenu = new ContextMenu(".menu");
+const contextMenu = new ContextMenu('.menu');
 contextMenu.add();
-document.body.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-    contextMenu.open(event);
+document.body.addEventListener('contextmenu', event => {
+  event.preventDefault();
+  contextMenu.open(event);
 });
--- src/menu.js
+++ src/menu.js
@@ -1,31 +1,31 @@
-import { Menu } from "./core/menu";
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
-    constructor(selector) {
-        super(selector);
-    }
+  constructor(selector) {
+    super(selector);
+  }
 
-    add(...modules) {
-        for (const module of modules) {
-            this.el.insertAdjacentHTML("beforeend", module.toHTML());
-            module.addEventListener("click", module.trigger());
-        }
+  add(...modules) {
+    for (const module of modules) {
+      this.el.insertAdjacentHTML('beforeend', module.toHTML());
+      module.addEventListener('click', module.trigger());
     }
-    open(event) {
-        const parentWidth = this.el.parentNode.clientWidth;
-        const parentHeight = this.el.parentNode.clientHeight;
-        if (this.el.hasChildNodes()) {
-            if (event.clientX + this.el.clientWidth > parentWidth)
-                this.el.style.left = `${parentWidth - this.el.clientWidth}px`;
-            else this.el.style.left = `${event.clientX}px`;
-            if (event.clientY + this.el.clientHeight > parentHeight)
-                this.el.style.top = `${parentHeight - this.el.clientHeight}px`;
-            else this.el.style.top = `${event.clientY}px`;
+  }
+  open(event) {
+    const parentWidth = this.el.parentNode.clientWidth;
+    const parentHeight = this.el.parentNode.clientHeight;
+    if (this.el.hasChildNodes()) {
+      if (event.clientX + this.el.clientWidth > parentWidth)
+        this.el.style.left = `${parentWidth - this.el.clientWidth}px`;
+      else this.el.style.left = `${event.clientX}px`;
+      if (event.clientY + this.el.clientHeight > parentHeight)
+        this.el.style.top = `${parentHeight - this.el.clientHeight}px`;
+      else this.el.style.top = `${event.clientY}px`;
 
-            this.el.classList.add("open");
-        }
+      this.el.classList.add('open');
     }
-    close() {
-        this.el.classList.remove("open");
-    }
+  }
+  close() {
+    this.el.classList.remove('open');
+  }
 }
