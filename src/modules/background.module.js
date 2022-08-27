1256a8019356846e14ac4a4e5c30187272925471
MoranaMara <jenaodessita@gmail.com>
2022-08-27 17:00:51 +0300

    init background modul

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,13 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class BackgroundModule extends Module {
-
+  trigger() {
+    const letters = "ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+      color += letters[Math.floor(Math.random() * 6)];
+    }
+    document.body.style.backgroundColor = color;
+    return color;
+  }
 }
