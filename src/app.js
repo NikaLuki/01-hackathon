49bfabc65fe57be781cb7c84b1ccc51c19d6d9a5
Danil Grechkin <grechkin-danil@mail.ru>
2022-08-27 16:13:39 +0300

    delet str callmetod trigger

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const background = new BackgroundModule('test', 'test');
 const randomFrigure = new RandomFigure('random-figure', 'Случайная фигура')
 contextMenu.add(background);
 contextMenu.add(randomFrigure);
-randomFrigure.trigger() // вызов метода, чтобы при клике на модуль происходило событие
 document.body.addEventListener('contextmenu', event => {
   event.preventDefault();
   contextMenu.open(event);
