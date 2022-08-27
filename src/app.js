d87475d6d24310d334c71613711934793f815081
Dmitriy0642 <jesusdontdie@gmail.com>
2022-08-27 17:02:12 +0300

    complate-counter-block

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import './styles.css';
 import { ContextMenu } from './menu';
 import { BackgroundModule } from './modules/background.module';
+import {ClicksModule} from './modules/clicks.module'
 
 const contextMenu = new ContextMenu('.menu');
 const background = new BackgroundModule('test', 'test');
+const clickModuleText = new ClicksModule('clickModule', 'Подсчет кликов за 3 секунды')
 contextMenu.add(background);
+contextMenu.add(clickModuleText)
 document.body.addEventListener('contextmenu', event => {
   event.preventDefault();
   contextMenu.open(event);
 });
+
+
+
+
+
+
+
+
