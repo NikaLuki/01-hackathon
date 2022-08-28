cd6627e4f41fa6206cacaf40772f29cd91278419
MoranaMara <jenaodessita@gmail.com>
2022-08-28 12:37:46 +0300

    color-setInterval

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,12 +2,14 @@ import { Module } from "../core/module";
 
 export class BackgroundModule extends Module {
   trigger() {
-    const letters = "ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 3; i++) {
-      color += letters[Math.floor(Math.random() * 6)];
-    }
-    document.body.style.backgroundColor = color;
-    return color;
-  } 
+    setInterval(function () {
+      const letters = "ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 3; i++) {
+        color += letters[Math.floor(Math.random() * 6)];
+      }
+      document.body.style.backgroundColor = color;
+      return color;
+    }, 5000);
+  }
 }
